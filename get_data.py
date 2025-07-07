c269c88860e8a7800de005f691bf7eba19aaf30d
Emily Zhao <2023zhaoe83@gmail.com>
2025-07-07 16:45:47 -0400

    stream

--- get_data.py
+++ get_data.py
@@ -5,10 +5,13 @@ from runeq import initialize
 from runeq.resources.stream_metadata import get_patient_stream_metadata
 import time 
 
+
+
+
 #init
 initialize()
 
-patient_id = "6KOZ" #insert rune labs patient ID
+patient_id = "" #insert rune labs patient ID
 
 #try to only pull 1 patient at a time, for limited time
 patient_streams = get_patient_stream_metadata(patient_id)
@@ -28,12 +31,6 @@ device_streams = patient_streams.filter(
     filter_function=most_recent_two_weeks
 )
 
-# recent_lfp_streams = patient_streams.filter(
-#     category = "neural",
-#     device_id=device_id,
-#     filter_function = most_recent_two_weeks
-# )
-
 #define time window:
 end_time = int(time.time())
 start_time = end_time - 14*24*60*60 #current time minus 14 days (*24 hours * 60 minutes * 60 seconds)
@@ -50,3 +47,11 @@ os.makedirs(output_dir, exist_ok=True)
 output_path = os.path.join(output_dir, "testneural.json")
 stream_df.to_json(output_path, orient="records", indent=2)
 print(f"patient data exported to: {output_path}")
+
+
+
+#From Rune Labs API documentation: 
+# patient_id = "c4bd060df1454aa0adc978985512c6e9"
+# patient_streams = get_patient_stream_metadata(patient_id)
+# print(f'Found {len(patient_streams)} streams')
+#outputs how many streams per patientID
